219835a09fb28017d77ce9985da14681dae07d78
Celdama <celdama.dev@gmail.com>
2021-12-28 11:48:48 +0100

    add global style component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-function App() {
+// styles
+import { GlobalStyle } from './GlobalStyle';
+
+const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       Start here.
+      <GlobalStyle />
     </div>
   );
-}
+};
 
 export default App;
