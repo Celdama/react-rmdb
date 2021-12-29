cfeaafd2902031e1086e271c2740321456d04bbc
Celdama <celdama.dev@gmail.com>
2021-12-29 09:11:16 +0100

    components/Home: use custome hook in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,11 +9,14 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 
 // Hook
+import { useHomeFetch } from '../hooks/useHomeFetch';
 
 // Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
+  const { state, loading, error } = useHomeFetch();
+
   console.log(state);
 
   return <div>Home Page</div>;
