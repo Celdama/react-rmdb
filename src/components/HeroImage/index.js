abd28c498535aeb31c765243583e39ff3ccaebd4
Celdama <celdama.dev@gmail.com>
2021-12-29 09:23:44 +0100

    components/Home:  using HeroImage component in home page

--- src/components/HeroImage/index.js
+++ src/components/HeroImage/index.js
@@ -3,12 +3,12 @@ import React from 'react';
 // Styles
 import { Wrapper, Content, Text } from './HeroImage.styles';
 
-const HeroImage = (props) => (
-  <Wrapper image={props.image}>
+const HeroImage = ({ image, title, text }) => (
+  <Wrapper image={image}>
     <Content>
       <Text>
-        <h1>{props.title}</h1>
-        <p>{props.text}</p>
+        <h1>{title}</h1>
+        <p>{text}</p>
       </Text>
     </Content>
   </Wrapper>
