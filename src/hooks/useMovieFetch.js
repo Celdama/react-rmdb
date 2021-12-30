38f1677d572fce1ace35997d22c374dc236a72f7
Celdama <celdama.dev@gmail.com>
2021-12-30 11:28:26 +0100

    create BreadCrumb component

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import API from '../API';
 
 export const useMovieFetch = (movieId) => {
@@ -6,29 +6,37 @@ export const useMovieFetch = (movieId) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        setError(false);
+  const fetchMovie = useCallback(async () => {
+    try {
+      setLoading(true);
+      setError(false);
 
-        const movie = await API.fetchMovie(movieId);
-        const credits = await API.fetchCredits(movieId);
-        // Get directors only
-        const directors = credits.crew.filter(
-          (member) => member.job === 'Director'
-        );
+      const movie = await API.fetchMovie(movieId);
+      const credits = await API.fetchCredits(movieId);
+      // Get directors only
+      const directors = credits.crew.filter(
+        (member) => member.job === 'Director'
+      );
 
-        setState({
-          ...movie,
-          actors: credits.cast,
-          directors,
-        });
+      setState({
+        ...movie,
+        actors: credits.cast,
+        directors,
+      });
 
-        setLoading(false);
-      } catch (error) {
-        setError(true);
-      }
-    };
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+    }
   }, [movieId]);
+
+  useEffect(() => {
+    fetchMovie();
+  }, [movieId, fetchMovie]);
+
+  return {
+    state,
+    loading,
+    error,
+  };
 };
