b0d2016644664ee442bb1ae316575a43b19a2d9e
Celdama <celdama.dev@gmail.com>
2021-12-31 16:44:44 +0100

    components/MovieInfo/index: create content for MovieInfo component

--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -8,20 +8,49 @@ import NoImage from '../../images/no_image.jpg';
 // Styles
 import { Wrapper, Content, Text } from './MovieInfo.styles';
 
-const MovieInfo = ({ movie }) => (
-  <Wrapper backdrop={movie.backdrop_path}>
-    <Content>
-      <Thumb
-        image={
-          movie.poster_path
-            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
-            : NoImage
-        }
-        clickable={false}
-        alt='movie-thumb'
-      ></Thumb>
-    </Content>
-  </Wrapper>
-);
+const MovieInfo = ({ movie }) => {
+  const {
+    backdrop_path,
+    poster_path,
+    title,
+    overview,
+    vote_average,
+    directors,
+  } = movie;
+
+  return (
+    <Wrapper backdrop={backdrop_path}>
+      <Content>
+        <Thumb
+          image={
+            poster_path
+              ? `${IMAGE_BASE_URL}${POSTER_SIZE}${poster_path}`
+              : NoImage
+          }
+          clickable={false}
+          alt='movie-thumb'
+        ></Thumb>
+        <Text>
+          <h1>{title}</h1>
+          <h3>PLOT</h3>
+          <p>{overview}</p>
+
+          <div className='rating-director'>
+            <div>
+              <h3>RATING</h3>
+              <div className='score'>{vote_average}</div>
+            </div>
+            <div className='director'>
+              <h3>DIRECTOR{directors.length > 1 ? 'S' : ''}</h3>
+              {directors.map(({ credit_id, name }) => (
+                <p key={credit_id}>{name}</p>
+              ))}
+            </div>
+          </div>
+        </Text>
+      </Content>
+    </Wrapper>
+  );
+};
 
 export default MovieInfo;
